ce2f6a1a903879419215714ff2961cd4dfa60ca3
Ryukizi <niellealbrandao@gmail.com>
2025-02-20 00:24:32 -0300

    :tada:

--- Assets/Scripts/Jogo.cs
+++ Assets/Scripts/Jogo.cs
@@ -4,20 +4,13 @@ using UnityEngine.SceneManagement;
 public class Jogo : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    private int pontos;
-    public int outrosPontos;
-    public float peso;
-
+    
     public GameObject painelDerrota;
     public GameObject painelVitoria;
 
     public bool jogadorFoiPego = false;
     public bool jogadorEscapou = false; 
     
-    void Start  {
-        Debug.Log("importante variaveis")
-    }
     // Update is called once per frame
     void Update()
     {
